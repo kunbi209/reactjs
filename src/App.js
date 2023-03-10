247ca0fc80b7f7da7715c97e2bf623714957c6ef
Tien Toan <trandttoan@gmail.com>
2023-03-10 20:55:42 +0700

    props study

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import TodoFeature from './features/Todo';
 
 function App() {
+  const name = 'Tien Toan';
+  const age = 18;
+  const isGioTinh = true;
+  const student = {
+    name: 'Tien Toan',
+    age: 30,
+    gioiTinh: true,
+    soThich: 'Game, xem phim'
+  };
+  const colorList = ['red', 'green', 'blue']
   return (
     <div className="App">
       <header className="App-header">
@@ -9,6 +21,35 @@ function App() {
         <p>
           Tien Toan 123 pro vip kk
         </p>
+        <p>Xin chao {name} - {age} - {isGioTinh ? 'Nam' : 'Nu'}</p>
+        {isGioTinh ? <p>Nam</p> : <p>Nữ</p>}
+        {isGioTinh && <p>Nam</p>}
+        {!isGioTinh && <p>Nữ</p>}
+        {isGioTinh && (
+          <div className='abc'>
+            <p>Nam 1</p>
+            <p>Nam 2</p>
+          </div>
+        )}
+        {isGioTinh && (
+          <React.Fragment>
+            <p>Nam 1</p>
+            <p>Nam 2</p>
+          </React.Fragment>
+        )}
+        {isGioTinh && (
+          <>
+            <p>Nam 3</p>
+            <p>Nam 4</p>
+          </>
+        )}
+        <p>{student.name} - {student.soThich}</p>
+        <ul>
+            {colorList.map( color =>  (
+              <li style={{color}}>{color}</li>
+            ))}
+        </ul>
+        <TodoFeature/>
       </header>
     </div>
   );
---
+++ src/commponents/Footer/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './style.scss';
+
+Footer.propTypes = {
+    
+};
+
+function Footer(props) {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default Footer;
