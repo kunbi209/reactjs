aa9229a255bac97be41d1e142250cca611dd279b
Tien Toan <trandttoan@gmail.com>
2023-03-14 17:16:07 +0700

    router-link

--- src/App.js
+++ src/App.js
@@ -1,63 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
 import React from 'react';
-import TodoFeature from './features/Todo';
-import AlbumFeature from './features/Album';
+import { NavLink, Route, Routes } from 'react-router-dom';
+import './App.css';
 import ColorBox from './commponents/ColorBox';
 import Counter from './commponents/Counter';
+import AlbumFeature from './features/Album';
+import TodoFeature from './features/Todo';
 
 function App() {
-  const name = 'Tien Toan';
-  const age = 18;
-  const isGioTinh = true;
-  const student = {
-    name: 'Tien Toan',
-    age: 30,
-    gioiTinh: true,
-    soThich: 'Game, xem phim'
-  };
-  const colorList = ['red', 'green', 'blue']
   return (
     <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Tien Toan 123 pro vip kk
-        </p>
-        <p>Xin chao {name} - {age} - {isGioTinh ? 'Nam' : 'Nu'}</p>
-        {isGioTinh ? <p>Nam</p> : <p>Nữ</p>}
-        {isGioTinh && <p>Nam</p>}
-        {!isGioTinh && <p>Nữ</p>}
-        {isGioTinh && (
-          <div className='abc'>
-            <p>Nam 1</p>
-            <p>Nam 2</p>
-          </div>
-        )}
-        {isGioTinh && (
-          <React.Fragment>
-            <p>Nam 1</p>
-            <p>Nam 2</p>
-          </React.Fragment>
-        )}
-        {isGioTinh && (
-          <>
-            <p>Nam 3</p>
-            <p>Nam 4</p>
-          </>
-        )}
-        <p>{student.name} - {student.soThich}</p>
-        <ul>
-          {colorList.map(color => (
-            <li style={{ color }}>{color}</li>
-          ))}
-        </ul>
-        <TodoFeature />
-      </header> */}
-      <AlbumFeature />
-      <ColorBox />
-      <Counter />
-    </div>
+        Header
+        <p><NavLink to="/">/</NavLink></p>
+        <p><NavLink to="/todos">Todos</NavLink></p>
+        <p><NavLink to="/albums">Albums</NavLink></p>
+        <p><NavLink to="/colorBox">ColorBox</NavLink></p>
+        <p><NavLink to="/counter">Counter</NavLink></p>
+
+          <Routes>
+              <Route path='/todos' Component={TodoFeature}/>
+              <Route path='/' Component={AlbumFeature}/>
+              <Route path='/albums' Component={AlbumFeature}/>
+              <Route path='/colorBox' Component={ColorBox}/>
+              <Route path='/counter' Component={Counter}/>
+          </Routes>
+        Footer
+      </div>
+    
   );
 }
 
